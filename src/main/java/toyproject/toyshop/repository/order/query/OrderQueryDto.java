1713036d2c56aeebd8de9ad3b6211ff7830a90aa
dlrbcnvk <dlrbcnvk@naver.com>
2021-09-04 18:12:40 +0900

    210904/18:09 회원, 주문 api 만들어 보았다. 이제 springData JPA, Querydsl 해볼 차례

---
+++ src/main/java/toyproject/toyshop/repository/order/query/OrderQueryDto.java
@@ -0,0 +1,28 @@
+package toyproject.toyshop.repository.order.query;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import toyproject.toyshop.domain.Address;
+import toyproject.toyshop.domain.OrderStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(of = "orderId")
+public class OrderQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemQueryDto> orderItems;
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
